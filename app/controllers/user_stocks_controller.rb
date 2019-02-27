a3cde684800e11be5898ff932b3b5ccfae8fc196
mattorus <mattorus@gmail.com>
2019-02-27 08:31:28 -0800

    Add list view for user stocks; Enable removal of stocks from portfolio.

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -10,6 +10,14 @@ class UserStocksController < ApplicationController
     @user_stock = UserStock.create(user: current_user, stock: stock)
     flash[:success] = "Stock #{@user_stock.stock.name} was successfully added to portfolio"
     redirect_to my_portfolio_path
+  end
+
+  def destroy
+    stock = Stock.find(params[:id])
+    @user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
+    @user_stock.destroy
+    flash[:notice] = "Stock was successfully removed from portfolio"
+    redirect_to my_portfolio_path
 
   end
 
